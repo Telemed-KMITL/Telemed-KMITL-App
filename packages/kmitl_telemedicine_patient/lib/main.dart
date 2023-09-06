3c443acd7244abe10938fbc35b7d90ead29fc7df
sthairno <45668272+sthairno@users.noreply.github.com>
2023-09-06 14:42:42 +0700

    Fix errors while building android app

--- packages/kmitl_telemedicine_patient/lib/main.dart
+++ packages/kmitl_telemedicine_patient/lib/main.dart
@@ -8,6 +8,7 @@ import "package:kmitl_telemedicine_patient/router.dart";
 import "firebase_options.dart";
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
